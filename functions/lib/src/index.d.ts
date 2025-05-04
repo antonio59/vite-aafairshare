43648c6f67a5ba4ffa383c5946119eb85dc851f8
Antonio Smith <antoniosmith@gmail.com>
2025-05-05 00:58:28 +0100

    fix(functions): deploy to europe-west1, remove unused imports, and update CI to npm only

--- functions/lib/src/index.d.ts
+++ functions/lib/src/index.d.ts
@@ -1,4 +1,4 @@
 import * as functions from "firebase-functions";
-import * as firestoreV1 from "firebase-functions/v1/firestore";
-export declare const onSettlementCreated: import("firebase-functions/lib/v1/cloud-functions.js").CloudFunction<firestoreV1.QueryDocumentSnapshot>;
-export declare const onSettlementMarkedSettled: import("firebase-functions/lib/v1/cloud-functions.js").CloudFunction<functions.firestore.Change<firestoreV1.QueryDocumentSnapshot>>;
+import * as functionsV1 from "firebase-functions/v1";
+export declare const onSettlementCreated: functionsV1.CloudFunction<functionsV1.firestore.QueryDocumentSnapshot>;
+export declare const onSettlementMarkedSettled: functionsV1.CloudFunction<functions.firestore.Change<functionsV1.firestore.QueryDocumentSnapshot>>;
